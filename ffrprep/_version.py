c4079e9fcc35cbdffc8713d1c8829b1a5cff52e3
PeerHerholz <herholz.peer@gmail.com>
2024-12-27 12:09:31 +0100

    set package name

--- ffrprep/_version.py
+++ ffrprep/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440-post"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "package_name/_version.py"
+    cfg.versionfile_source = "ffrprep/_version.py"
     cfg.verbose = False
     return cfg
 
