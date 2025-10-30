548db787c7c290a4c1c7b44c404e381e73eed3ef
PeerHerholz <herholz.peer@gmail.com>
2025-10-30 11:06:33 +0100

    update _version.py to fix workflow error

--- ffrprep/_version.py
+++ ffrprep/_version.py
@@ -314,8 +314,8 @@ def git_pieces_from_vcs(tag_prefix, root, verbose, run_command=run_command):
 
     # commit date: see ISO-8601 comment in git_versions_from_keywords()
     date = run_command(GITS, ["show", "-s", "--format=%ci", "HEAD"],
-                       cwd=root)
-    pieces["date"] = date.strip().replace(" ", "T", 1).replace(" ", "", 1)
+                       cwd=root)[0].strip()
+    pieces["date"] = date.replace(" ", "T", 1).replace(" ", "", 1)
 
     return pieces
 
