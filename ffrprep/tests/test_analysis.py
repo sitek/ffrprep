68751ae10ff8627847b54ac82c760e17d11efff1
Kevin Sitek <sitek.kevin@gmail.com>
2025-11-20 17:15:03 -0600

    Added tests for pitch tracking

--- ffrprep/tests/test_analysis.py
+++ ffrprep/tests/test_analysis.py
@@ -71,3 +71,43 @@ def test_autocorrelation(osf_url, tmp_path):
 
     # Test function with defaults
     autocorrelation(evoked)
+
+def test_compute_pitch_and_conf(osf_url, tmp_path):
+    # Use temporary directory for testing
+    dataset_path = tmp_path / "test_dataset"
+
+    # Download and unzip the data
+    data_path = download_unzip_exp_data(osf_url, dataset_path)
+
+    eeg_data = load_data(bids_root=data_path,
+                         sub_label='21',
+                         session_label=None,
+                         task_label='passive',
+                         run_label=1,)
+
+    epochs = epoch_data(eeg_data, baseline=[-0.05, 0.2])
+    evoked = make_evoked(epochs=epochs, by_event_type=True)
+
+    # Test function with defaults
+    from ffrprep.analysis import compute_pitch_and_conf
+    compute_pitch_and_conf(evoked)
+
+def test_plot_pitch_track(osf_url, tmp_path):
+    # Use temporary directory for testing
+    dataset_path = tmp_path / "test_dataset"
+
+    # Download and unzip the data
+    data_path = download_unzip_exp_data(osf_url, dataset_path)
+
+    eeg_data = load_data(bids_root=data_path,
+                         sub_label='21',
+                         session_label=None,
+                         task_label='passive',
+                         run_label=1,)
+
+    epochs = epoch_data(eeg_data, baseline=[-0.05, 0.2])
+    evoked = make_evoked(epochs=epochs, by_event_type=True)
+
+    from ffrprep.analysis import compute_pitch_and_conf, plot_pitch_track
+    pitch_smooth, conf_rmax = compute_pitch_and_conf(evoked)
+    plot_pitch_track(evoked.times, pitch_smooth, conf_rmax)
